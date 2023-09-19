057328dfc6114c57826ada96ae0dd79274b49485
CollinJH <cjchargreaves@gmail.com>
2023-09-19 11:59:32 -0700

    project

--- lab4/question8.py
+++ lab4/question8.py
@@ -7,12 +7,29 @@ def ten_pairs(n):
     >>> ten_pairs(9641469)
     6
     """
-    def ten_pairs_counter(n, target):
+    def ten_pairs_counter(n, i, j, counter):
+        str_n = str(n)
+        length_n = len(str_n)
+        
+        # base case, last digit has been reached
+        if i == length_n - 1:
+            return counter
+
+
+        digit_to_add_n = (n // (10 ** i)) % 10
+        if digit_to_add_n + int(str_n[j]) == 10:
+            counter += 1
+        
+        if j == length_n - 1:
+            return ten_pairs_counter(n, i + 1, i + 2, counter)
+
+        return ten_pairs_counter(n, i, j + 1, counter)
+            
             
 
 
 
 
 
-    return ten_pairs_counter(n, target = 0)
+    return ten_pairs_counter(n, i = 0, j = 1, counter = 0)
             
--- lab4/test.py
+++ lab4/test.py
@@ -1,7 +1,7 @@
 
 
 
-source = 82537
+source = 55055
 source_str = str(source)
 length = len(source_str)
 digit_to_add = 0
@@ -35,4 +35,29 @@ while i < length:
     i += 1
     n = 1 
 
-print(counter)
+# iterative solution
+def count_ten_pairs(n):
+    str_n = str(n)
+    length_n = len(str_n)
+    digit_to_add_n = 0
+    m = 1
+    i = 0
+    j = 0
+    counter = 0
+    while i < length_n:
+        j = i + 1
+        while j < length_n:
+            digit_to_add_n += (source // m) % 10
+            if digit_to_add_n + int(str_n[i]) == 10:
+                counter += 1
+            digit_to_add_n = 0
+            m *= 10
+            j += 1
+        i += 1
+        m = 1
+
+    return counter
+
+a = count_ten_pairs(55055)
+
+print(a)
